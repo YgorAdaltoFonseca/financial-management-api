2c905ac36eca94e8d84366437c943d5f0b726701
Ygor Adalto <ygor8920@gmail.com>
2026-08-26 14:53:51 -0300

    feat: addition of the controllers

---
+++ src/main/java/side/financialmanagementapi/controller/UserController.java
@@ -0,0 +1,12 @@
+package side.financialmanagementapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+}
